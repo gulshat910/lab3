e393ae9bedce0821f3be365164b05183c4df51f5
gulshat910 <gulshatdulatova9@gmail.com>
2025-05-02 12:19:17 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,21 +1,22 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age): #Инициализация нового экземпляра класса Person
         self.name = name
         self.age = age
 
 class Teacher(Person):
-    def __init__(self, name, age, subject):
-        super().__init__(name, age)
-        self.subject = subject
-        self.students = []
+    def __init__(self, name, age, subject): #Инициализация нового экземпляра класса Teacher
+        super().__init__(name, age) #Вызов конструктора базового класса Person
+        self.subject = subject #Преподаваемый предмет
+        self.students = [] #Список студентов
 
-    def add_student(self, student):
+    def add_student(self, student): #Добавляет студента в список студентов преподавателя
         if student not in self.students:
             self.students.append(student)
 
-    def remove_student(self, student):
+    def remove_student(self, student): #Удаляет студента из списка студентов преподавателя
         if student in self.students:
             self.students.remove(student)
 
-    def list_students(self):
+    def list_students(self): #Возвращает список студентов преподавателя
         return self.students
+
