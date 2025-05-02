a4b5b2040ebde7cdea615e7781bb5b8d3332635d
gulshat910 <gulshatdulatova9@gmail.com>
2025-05-02 12:09:02 +0300

    Update 1.5.py

--- 1.5.py
+++ 1.5.py
@@ -1,33 +1,33 @@
 class Student:
-    def __init__(self, name, student_id):
+    def __init__(self, name, student_id): #Инициализация нового экземпляра класса Student
         self.name = name
         self.student_id = student_id
-        self.grades = []
+        self.grades = [] #Список оценок студента
 
-    def add_grade(self, grade):
+    def add_grade(self, grade): #Добавление оценки с проверкой валидности
         if 0 <= grade <= 10:
             self.grades.append(grade)
         else:
-            raise ValueError("Оценка должна быть от 0 до 10")
+            raise ValueError("Оценка должна быть в диапазоне от 0 до 10") #Ошибка,если оценка выходит за пределы диапазона 0-10
 
-    def get_average(self):
-        if not self.grades:
+    def get_average(self): #Возвращает средний балл студента
+        if len(self.grades) == 0:
             return 0.0
         return sum(self.grades) / len(self.grades)
 
-    def display_info(self):
-        print("Имя: {self.name}")
-        print("ID студента: {self.student_id}")
-        print("Оценки: {', '.join(map(str, self.grades))}")
-        print("Средний балл: {self.get_average()}")
+    def display_info(self): # Выводит информацию о студенте в удобном формате
+        print(f"Имя: {self.name}")
+        print(f"ID студента: {self.student_id}")
+        print(f"Оценки: {', '.join(map(str, self.grades))}")
+        print(f"Средний балл: {self.get_average()}")
 
-    def __str__(self):
-        return f"Студент({self.name}, {self.student_id}, {self.grades})"
+    def __str__(self): #Cтроковое представление объекта Student
+        return f"Студент {self.name} (ID: {self.student_id})"
 
-    def __eq__(self, other):
+    def __eq__(self, other): #Сравнивает два объекта Student по их student_id
         if isinstance(other, Student):
             return self.student_id == other.student_id
         return False
 
-    def __len__(self):
+    def __len__(self): #Возвращает количество оценок у студента
         return len(self.grades)
